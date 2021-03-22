11a2ad494a9b5b24bb0c7d6e44042573aebfe069
Jo Clegg <joseph.clegg@geniussports.com>
2021-03-22 00:04:19 +0000

    Add simple build workflow

--- GoogleSearchResultScraperUI/src/components/App/App.tsx
+++ GoogleSearchResultScraperUI/src/components/App/App.tsx
@@ -8,16 +8,7 @@ const App = (): React.ReactElement => {
 
   return (
     <div className={classes.root}>
-
     <SearchForm />
-      {/* <Button className={classes.button} variant="contained" color="primary" onClick={incrementCounter}>
-        Click me
-      </Button>
-      <Button className={classes.button} variant="contained" color="primary" onClick={getWinner}>
-        Show winner
-      </Button>
-      <Typography className={classes.summary}>Button is clicked {store.counter} times.</Typography>
-      <Typography>Random winner is {getFullName(store.winner)}.</Typography> */}
     </div>
   );
 };
--- GoogleSearchResultScraperUI/src/components/App/AppStyles.ts
+++ GoogleSearchResultScraperUI/src/components/App/AppStyles.ts
@@ -1,17 +1,11 @@
 import {createStyles, makeStyles, Theme} from "@material-ui/core";
 
-declare type classNames = "root" | "button" | "summary";
+declare type classNames = "root";
 
 export default makeStyles<Theme, Record<never, never>, classNames>((theme) =>
   createStyles({
     root: {
       margin: theme.spacing(2),
-    },
-    button: {
-      marginRight: theme.spacing(),
-    },
-    summary: {
-      fontSize: 24,
-    },
+    }
   }),
 );
